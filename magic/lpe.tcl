e622b38a3efea861b96873cb8a5bd1cbcb18925c
Carsten Wulff <carsten@wulff.no>
2023-04-10 23:38:13 +0200

    Not found a pin order solution to lpe, need to find one, maybe a perl regex

--- magic/lpe.tcl
+++ magic/lpe.tcl
@@ -1,15 +1,14 @@
-
+#set VDD AVDD
+#set GND AVSS
+#set SUB 0
 load {PATH}/{CELL}.mag
 
 select top cell
 #- Tim does not trust hierarchy extract, but port order gets screwed up with flat
-#flatten {CELL}_flat
-#load {CELL}_flat
+flatten {CELL}_flat
+load {CELL}_flat
 
 extract all
-set VDD AVDD
-set GND AVSS
-set SUB AVSS
 
 ext2sim labels on
 ext2sim
@@ -17,8 +16,8 @@ ext2sim
 ext2spice lvs
 ext2spice format ngspice
 ext2spice resistor off
-ext2spice hierarchy off
-ext2spice subcircuits off
+#ext2spice hierarchy off
+#ext2spice subcircuits off
 #ext2spice merge conservative
 ext2spice cthresh 0.1
 ext2spice -o lpe/{CELL}_lpe.spi
