97b6dce4deab341c1741f9f967c3ead151a9b36a
Carsten Wulff <carsten@wulff.no>
2023-04-10 22:39:32 +0200

    Reverted lpe setup, the new one fubard the pin mapping

--- magic/lpe.tcl
+++ magic/lpe.tcl
@@ -1,14 +1,23 @@
+set VDD AVDD
+set GND AVSS
+set SUB 0
 load {PATH}/{CELL}.mag
 select top cell
-flatten {CELL}_flat
-load {CELL}_flat
+
+#- Tim does not trust hierarchy extract, but port order gets screwed up with flat
+#flatten {CELL}_flat
+#load {CELL}_flat
+
 extract all
 ext2sim labels on
 ext2sim
-extresist tolerance 10
-extresist
+
 ext2spice lvs
-ext2spice cthresh 0
-ext2spice extresist on
+ext2spice format ngspice
+ext2spice resistor off
+ext2spice hierarchy off
+ext2spice subcircuits off
+#ext2spice merge conservative
+ext2spice cthresh 0.1
 ext2spice -o lpe/{CELL}_lpe.spi
 quit
